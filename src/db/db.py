cc85c2ecd2448b139a2cfb9c2f2640e17932ac14
José Carlos Sánchez <a01174050@tec.mx>
2023-10-09 23:41:16 -0600

    feat: Connection to mongodb Atlas

--- src/db/db.py
+++ src/db/db.py
@@ -1,10 +1,15 @@
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 from decouple import config
+from pymongo.server_api import ServerApi
 
 MONGO_PORT = config("MONGO_PORT")
 MONGO_HOST = config("MONGO_HOST")
 MONGO_DBNAME = config("MONGO_DBNAME")
 
-client = MongoClient("mongodb://localhost:27017/")
+#client = MongoClient("mongodb://localhost:27017/")
+
+uri = "mongodb+srv://jcsg:SKxXLxVQOqdnHjFQ@cluster0.3cptiz2.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(uri, tlsAllowInvalidCertificates=True)
+client.admin.command('ismaster')
 db = client[MONGO_DBNAME]
